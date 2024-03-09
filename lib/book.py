78e393ebfc5b3e1ebc4657bda104a9c1752f8c7e
Eric Maranga <ekirira22@gmail.com>
2024-03-09 22:07:56 +0300

    completed assessment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
+import ipdb
+
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title="And Then There Were None", page_count=272):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+spiritual_world = Book("Spiritual World, By Peter Tan", 100)
+print(spiritual_world.title)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="Adidas", size=9):
+        self.brand = brand
+        self.size = size
+
+    condition = "New"
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
+
+erics_shoe = Shoe()
+print(erics_shoe.size)
